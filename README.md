fa88fd87673dca3b2734bcb1dbbf8ac5989b768f
Zeke Gabrielse <ezekg@yahoo.com>
2021-08-23 08:08:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Example Release Upload
+
+This is an example of [creating a new release](https://keygen.sh/docs/api/#releases)
+and [uploading an artifact](https://keygen.sh/docs/api/#releases-artifacts-upload)
+using Keygen's distribution API.
+
+## Running the example
+
+First up, configure a few environment variables:
+
+```bash
+# Your Keygen product token. You can generate a product token via the API
+# or your admin dashboard.
+export KEYGEN_PRODUCT_TOKEN="A_KEYGEN_ACTIVATION_TOKEN"
+
+# Your Keygen account ID. Find yours at https://app.keygen.sh/settings.
+export KEYGEN_ACCOUNT_ID="YOUR_KEYGEN_ACCOUNT_ID"
+
+# Your Keygen product ID.
+export KEYGEN_PRODUCT_ID="YOUR_KEYGEN_ACCOUNT_ID"
+```
+
+You can either run each line above within your terminal session before
+starting the app, or you can add the above contents to your `~/.bashrc`
+file and then run `source ~/.bashrc` after saving the file.
+
+Next, install dependencies with [`pip`](https://packaging.python.org/):
+
+```
+pip install -r requirements.txt
+```
+
+To create and upload a new release, run the program:
+
+```
+python main.py
+```
+
+The script will upsert a new `1.0.0` release for `dist/hello-world.txt`,
+and upload the `file` to the release's artifact relationship.
+
+## Questions?
+
+Reach out at [support@keygen.sh](mailto:support@keygen.sh) if you have any
+questions or concerns!
---
+++ main.py
@@ -0,0 +1,105 @@
+import requests
+import json
+import sys
+import os
+
+def to_error_message(errs):
+  """
+  Formats an array of error dicts into an error message string. Returns an error message.
+  """
+
+  return ', '.join(map(lambda e: f"{e['title']}: {e['detail']}", errs))
+
+def create_release(**kwargs):
+  """
+  Creates a new release for the configured product. Returns a release object.
+  """
+
+  res = requests.put(
+    f"https://api.keygen.sh/v1/accounts/{os.environ['KEYGEN_ACCOUNT_ID']}/releases",
+    headers={
+      'Authorization': f"Bearer {os.environ['KEYGEN_PRODUCT_TOKEN']}",
+      'Content-Type': 'application/vnd.api+json',
+      'Accept': 'application/vnd.api+json'
+    },
+    data=json.dumps({
+      'data': {
+        'type': 'releases',
+        'attributes': {
+          **kwargs
+        },
+        'relationships': {
+          'product': {
+            'data': { 'type': 'product', 'id': os.environ['KEYGEN_PRODUCT_ID'] }
+          }
+        }
+      }
+    })
+  )
+
+  release = res.json()
+
+  if 'errors' in release:
+    errs = release['errors']
+
+    print(f'[error] Release failed: errors={to_error_message(errs)}',
+          file=sys.stderr)
+
+    sys.exit(1)
+  else:
+    print(f"[info] Created: release={release['data']['id']} link={release['data']['links']['self']}",
+          file=sys.stdout)
+
+  return release['data']
+
+def upload_artifact_for_release(**kwargs):
+  """
+  Uploads an artifact for the given release. Returns an artifact object.
+  """
+
+  res = requests.put(
+    f"https://api.keygen.sh/v1/accounts/{os.environ['KEYGEN_ACCOUNT_ID']}/releases/{kwargs['release_id']}/artifact",
+    allow_redirects=False,
+    headers={
+      'Authorization': f"Bearer {os.environ['KEYGEN_PRODUCT_TOKEN']}",
+      'Accept': 'application/vnd.api+json'
+    }
+  )
+
+  upload_url = res.headers['location']
+  artifact   = res.json()
+
+  if 'errors' in artifact:
+    errs = artifact['errors']
+
+    print(f'[error] Upload failed: errors={to_error_message(errs)}',
+          file=sys.stderr)
+
+    sys.exit(1)
+  else:
+    print(f"[info] Uploaded: artifact={artifact['data']['id']} link={artifact['data']['links']['self']}",
+          file=sys.stdout)
+
+  # Follow redirect and upload file to S3
+  requests.put(upload_url,
+    headers={ 'Content-Type': 'text/plain' },
+    data=kwargs['file']
+  )
+
+  return artifact['data']
+
+with open('examples/hello-world.txt', mode='r') as f:
+  stat    = os.stat(f.name)
+  release = create_release(
+    filename=f.name,
+    filesize=stat.st_size,
+    version='1.0.0',
+    filetype='txt',
+    platform=sys.platform,
+    channel='stable'
+  )
+
+  artifact = upload_artifact_for_release(
+    release_id=release['id'],
+    file=f
+  )
